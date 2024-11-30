c336a09879c3f5e56296d4ddf0471ba852681136
Gaurav-Salian <129827331+Gaurav-Salian@users.noreply.github.com>
2024-11-30 07:17:14 +0530

    test

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,6 @@
+// next.config.mjs
 export default {
-    reactStrictMode: true,
-  };
+  reactStrictMode: true,   // Optional: Enables React Strict Mode
+  output: 'export',        // Enable static export
+  trailingSlash: true,     // Optional: Adds trailing slashes to URLs
+};
