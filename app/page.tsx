e4b4d1f7ab77aa4ee0fb83e555f5f21f1752c4aa
Gaurav-Salian <129827331+Gaurav-Salian@users.noreply.github.com>
2024-12-18 07:55:42 +0530

    nav fix

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import dynamic from "next/dynamic";
 import { navItems } from "@/data";
 
 // Dynamically import client-only components with `ssr: false`
-// const FloatingNav = dynamic(() => import("@/components/ui/FloatingNavbar"), { ssr: false });
+const FloatingNav = dynamic(() => import("@/components/ui/FloatingNavbar").then(mod => mod.FloatingNav), { ssr: false });
 const Hero = dynamic(() => import("@/components/Hero"), { ssr: false });
 const Grid = dynamic(() => import("@/components/Grid"), { ssr: false });
 const RecentProjects = dynamic(() => import("@/components/RecentProjects"), { ssr: false });
@@ -11,8 +11,6 @@ const Clients = dynamic(() => import("@/components/Client"), { ssr: false });
 const Experience = dynamic(() => import("@/components/Experience"), { ssr: false });
 const Approach = dynamic(() => import("@/components/Approach"), { ssr: false });
 const Footer = dynamic(() => import("@/components/Footer"), { ssr: false });
-import { FloatingNav } from "@/components/ui/FloatingNavbar";
-
 
 export default function Home() {
   return (
